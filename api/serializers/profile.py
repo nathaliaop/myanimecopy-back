c1889788d13068d63221a11238f40544d64e1eb3
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-02-16 19:26:47 -0300

    fix: Informações de perfil não são mais retornadas dentro do parâmetro social

--- api/serializers/profile.py
+++ api/serializers/profile.py
@@ -3,11 +3,13 @@ from api.models.profile import Profile
 from api.serializers.animestatus import AnimeStatusSerializer
 from api.serializers.mangastatus import MangaStatusSerializer
 from api.serializers.moviestatus import MovieStatusSerializer
+from api.serializers.social import SocialSerializer
 
 class ProfileSerializer(serializers.ModelSerializer):
     animestatus = AnimeStatusSerializer(many=True)
     mangastatus = MangaStatusSerializer(many=True)
     moviestatus = MovieStatusSerializer(many=True)
+    social = SocialSerializer()
     class Meta:
         model = Profile
         fields = ['id', 'user', 'image', 'animestatus', 'mangastatus', 'moviestatus', 'social']
